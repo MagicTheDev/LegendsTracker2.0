e574b90483d9d6fdb7cfb7f820e6768ca1dc7792
matthew <matthewanderson3607@gmail.com>
2022-03-02 03:42:00 -0600

    reload to slash & owner only

--- main.py
+++ main.py
@@ -10,21 +10,6 @@ bot = commands.Bot(command_prefix=commands.when_mentioned,
     help_command=None)
 
 
-@bot.command(name='reload', hidden=True)
-async def _reload(ctx, *, module : str):
-    """Reloads a module."""
-    if ctx.message.author.id == 706149153431879760:
-        try:
-            bot.unload_extension(module)
-            bot.load_extension(module)
-        except:
-            await ctx.send('<a:no:862552093324083221> Could not reload module.')
-        else:
-            await ctx.send('<a:check:861157797134729256> Reloaded module successfully')
-    else:
-        await ctx.send("You aren't magic. <:PS_Noob:783126177970782228>")
-
-
 initial_extensions = (
             "Check.check",
             "Check.check_stats",
@@ -49,6 +34,29 @@ initial_extensions = (
             "patreon",
             "ctrack"
         )
+
+
+def autocomp_names(self, query: str):
+    has = []
+    for i in initial_extensions:
+        if query in i:
+            has.append(i)
+    return has
+
+@bot.slash_command(name='reload', hidden=True)
+@commands.is_owner()
+async def _reload(ctx, *, module : str = commands.Param(autocomplete=autocomp_names)):
+    """Reloads a module."""
+    if ctx.message.author.id == 706149153431879760:
+        try:
+            bot.unload_extension(module)
+            bot.load_extension(module)
+        except:
+            await ctx.send('<a:no:862552093324083221> Could not reload module.')
+        else:
+            await ctx.send('<a:check:861157797134729256> Reloaded module successfully')
+    else:
+        await ctx.send("You aren't magic. <:PS_Noob:783126177970782228>")
 
 
 
