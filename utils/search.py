44597e5ebf7e639b06672f9544076d813798779c
matthew <matthewanderson3607@gmail.com>
2022-04-24 06:18:01 -0500

    fix clan search fix on empty search

--- utils/search.py
+++ utils/search.py
@@ -100,21 +100,11 @@ async def search_name_with_tag(query):
 
 async def search_clans_with_tag(query):
     names = set()
-    if len(query) == 0:
-        results = ongoing_stats.find({})
-        limit = await ongoing_stats.count_documents(filter={})
-        for document in await results.to_list(length=limit):
-            names.add(document.get("clan") + " | " + document.get("clan_tag"))
-            if len(names) == 25:
-                return list(names)
 
     query = re.escape(query)
     results = ongoing_stats.find({"clan": {"$regex": f"^(?i).*{query}.*$"}})
-    limit = await ongoing_stats.count_documents(filter={"clan": {"$regex": f"^(?i).*{query}.*$"}})
-    for document in await results.to_list(length=limit):
+    for document in await results.to_list(length=25):
         names.add(document.get("clan") + " | " + document.get("clan_tag"))
-        if len(names) == 25:
-            return list(names)
     if list(names) != []:
         return list(names)
 
