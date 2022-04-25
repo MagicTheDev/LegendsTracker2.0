850aaaf2454aa688e81dfdb923fe4a0fc04adb6f
matthew <matthewanderson3607@gmail.com>
2022-04-25 12:24:22 -0500

    api update

--- legendapi.py
+++ legendapi.py
@@ -1,6 +1,7 @@
 import asyncio
 from typing import Optional
 import motor.motor_asyncio
+import nest_asyncio
 import uvicorn
 import aiohttp
 from datetime import datetime
@@ -22,7 +23,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_LOGIN = os.getenv("DB_LOGIN")
-from utils.helper import coc_client
+COC_EMAIL = os.getenv("BETA_COC_EMAIL")
+COC_PASSWORD = os.getenv("BETA_COC_PASSWORD")
+
 
 db_client = motor.motor_asyncio.AsyncIOMotorClient(DB_LOGIN)
 legends_stats = db_client.legends_stats
@@ -154,6 +157,56 @@ async def player(player_tag: str, request : Request, response: Response):
         "num_prev_season_days_tracked" : season_stats[11]
     }
 
+@app.post("/add/{player_tag}")
+@limiter.limit("10/second")
+async def player_add(player_tag: str, request : Request, response: Response):
+
+    try:
+        player = await coc_client.get_player(player_tag)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=404, detail="Invalid Player")
+
+    if str(player.league) != "Legend League":
+        raise HTTPException(status_code=404, detail="Cannot add players not in legends")
+
+    result = await ongoing_stats.find_one({"tag": player.tag})
+    if result is not None:
+        raise HTTPException(status_code=404, detail="Player already added")
+
+    clan_name = "No Clan"
+
+    if player.clan is not None:
+        clan_name = player.clan.name
+
+    if player.clan is not None:
+        badge = player.clan.badge.url
+    else:
+        badge = clan_name
+    await ongoing_stats.insert_one({
+        "tag": player.tag,
+        "name": player.name,
+        "trophies": player.trophies,
+        "th": 14,
+        "num_season_hits": player.attack_wins,
+        "num_season_defenses": player.defense_wins,
+        "row_triple": 0,
+        "today_hits": [],
+        "today_defenses": [],
+        "num_today_hits": 0,
+        "previous_hits": [],
+        "previous_defenses": [],
+        "num_yesterday_hits": 0,
+        "servers": [],
+        "end_of_day": [],
+        "clan": clan_name,
+        "badge": badge,
+        "link": player.share_link,
+        "league": str(player.league),
+        "highest_streak": 0,
+        "last_updated": None,
+        "change": None
+    })
 
 @app.get("/all_tags")
 @limiter.limit("10/minute")
@@ -287,5 +340,9 @@ def season_hit_stats(player):
 
 
 if __name__ == '__main__':
+    nest_asyncio.apply()
+    loop = asyncio.new_event_loop()
+    coc_client = coc.login(COC_EMAIL, COC_PASSWORD, client=coc.EventsClient, realtime=True, loop= loop, key_count=10, key_names="DiscordBot",
+                           throttle_limit=25)
 
     uvicorn.run("legendapi:app", port=8000, host='45.33.3.218')
